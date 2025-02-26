c9fce1a338c806d8d518dff750877c3f9c2b8c40
Konstanca Nikolajevic <konstanca.nikolajevic@gmail.com>
2025-02-26 21:04:12 +0100

    migration effectuee

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_26_112446) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_25_194233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,7 +38,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_26_112446) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo_url"
     t.index ["user_id"], name: "index_mercenaries_on_user_id"
   end
 
