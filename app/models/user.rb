9ab942fef30e5fcc953cacc8ad619976b3d927c7
Vincent de Cabissole <vince2k@protonmail.com>
2025-02-26 17:43:10 +0100

    added active_storage migration, cloudinary setup, photo input in edit&new for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :bookings
   # voir si last_name first name et/ou nickname doivent être validés
 
+  has_one_attached :photo
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
