28eef0c3ac67b88a913a72dd46bbc7e1d3469fb0
Vincent de Cabissole <vince2k@protonmail.com>
2025-02-27 19:34:42 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :mercenaries
   has_many :bookings
-  validates :email, presence: true, uniqueness: true
 
   # voir si last_name first name et/ou nickname doivent être validés
 
