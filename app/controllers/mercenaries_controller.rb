d347c61f3496471f91533b7f08a5b1790acd45fa
ANGELICA IBARRA <47754843+Angiei@users.noreply.github.com>
2025-02-28 17:52:49 +0100

    WIP: Avancement CRUD Mercenaries

--- app/controllers/mercenaries_controller.rb
+++ app/controllers/mercenaries_controller.rb
@@ -1,5 +1,6 @@
 class MercenariesController < ApplicationController
   before_action :authenticate_user! # Devise s’assure que l’utilisateur est connecté avant d’accéder aux actions
+  before_action :set_mercenary, only: %i[show edit update destroy]
 
   def new
     @mercenary = Mercenary.new
@@ -7,7 +8,6 @@ class MercenariesController < ApplicationController
 
   def create
     @mercenary = current_user.mercenaries.build(mercenary_params) # Associe directement le mercenaire à l’utilisateur connecté par devise
-
     if @mercenary.save
       redirect_to root_path, notice: "Mercenaire ajouté avec succès !"
     else
@@ -15,7 +15,32 @@ class MercenariesController < ApplicationController
     end
   end
 
+  def show
+    @reviews = @mercenary.reviews if defined?(Review) # Charge les reviews si la table Review existe
+  end
+
+  def edit
+  end
+
+  def update
+    if @mercenary.update(mercenary_params)
+      redirect_to @mercenary, notice: "Le profil du mercenaire a été mis à jour avec succès."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @mercenary.destroy!
+    redirect_to mercenaries_path, notice: "Le mercenaire a été supprimé avec succès.", status: :see_other
+  end
+
+
   private
+
+  def set_mercenary
+    @mercenary = Mercenary.find(params[:id])
+  end
 
   def mercenary_params
     params.require(:mercenary).permit(:name, :bio, :photo_url, :price_per_day, :address, :specialty)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :mercenaries, only: [:new, :create]
+  resources :mercenaries, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
