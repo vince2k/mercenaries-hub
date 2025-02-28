ea0047585a100079d3883101847c26b8bd4499f0
Vincent de Cabissole <vince2k@protonmail.com>
2025-02-28 17:31:57 +0100

    booking controller updated, new seed, booking controller, nav bar edit

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,21 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :index]
+  before_action :set_mercenary, only: [:new, :create] # Définit @mercenary à partir de :mercenary_id
 
   def index
-    @bookings = current_user.bookings
+    @bookings = current_user.bookings # Toutes les réservations de l'utilisateur connecté
   end
 
   def new
-    @booking = Booking.new
+    @booking = @mercenary.bookings.build # Initialise avec le mercenaire
   end
 
   def create
-    # @booking = current_user.bookings.build(booking_params) # Associe directement le mercenaire à l’utilisateur connecté par devise
+    @booking = @mercenary.bookings.build(booking_params) # Associe au mercenaire
+    @booking.user = current_user # Associe à l’utilisateur connecté via Devise
 
-    @booking = Booking.new(booking_params)
-    @booking.user = User.find(params[:booking][:user_id].to_i)
-    @booking.mercenary = Mercenary.find(params[:mercenary_id])
-
-    if @booking.save!
-      # TODO redirect_to la show de l'utilisateur ?? = index de booking d'un utilisateur
-      redirect_to root_path, notice: "Reservation ajoutée avec succès !"
+    if @booking.save
+      redirect_to bookings_path, notice: "Réservation ajoutée avec succès !"
     else
       render :new, status: :unprocessable_entity
     end
@@ -26,10 +23,11 @@ class BookingsController < ApplicationController
 
   private
 
-  def total_price
+  def set_mercenary
+    @mercenary = Mercenary.find(params[:mercenary_id])
   end
 
   def booking_params
-    params.require(:booking).permit(:mission_purpose, :mission_place, :start_date, :end_date, :total_price, :mercenary_id, :user_id)
+    params.require(:booking).permit(:mission_purpose, :mission_place, :start_date, :end_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  resources :bookings, only: [:index]
   resources :mercenaries, only: [:new, :create, :index] do
-    resources :bookings, only: [:new, :create, :index]
+    resources :bookings, only: [:new, :create]
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
