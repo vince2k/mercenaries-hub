b82b4a60a6876050d7ecd02c60c4d2878ba7ae2e
Konstanca Nikolajevic <konstanca.nikolajevic@gmail.com>
2025-03-02 14:47:50 +0100

    dependances corrigées dans models booking, review et mercenary

--- app/models/mercenary.rb
+++ app/models/mercenary.rb
@@ -1,6 +1,6 @@
 class Mercenary < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
 
   validates :name, :specialty, :price_per_day, presence: true
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  belongs_to :booking
+  belongs_to :booking, dependent: :destroy
 
   validates :rating, presence: true, inclusion: { in: 1..5 }
 end
