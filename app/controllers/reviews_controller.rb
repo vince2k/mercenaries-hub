0e094105c411ab4cc2d44652ff784d009f594fa7
Konstanca Nikolajevic <konstanca.nikolajevic@gmail.com>
2025-03-02 16:56:47 +0100

    reviews controller updated with index and new methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,10 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = @mercenary.reviews # Toutes les revues du mercenaire
+  end
+
+  def new
+    @review = @booking.reviews.build # Initialise avec le booking
+  end
+
 end
