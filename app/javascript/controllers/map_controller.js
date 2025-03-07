7b94dfabedc4afc701f6110a41e063ea1c34ee38
Ihman Monrazel <monrazeli@gmail.com>
2025-03-07 20:07:27 +0100

    modification de la barre de recherche et map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -8,43 +8,45 @@ export default class extends Controller {
   }
 
   connect() {
-    console.log("Map controller connected");
+    console.log("Map controller connected!");
 
     mapboxgl.accessToken = this.apiKeyValue;
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v11"
+      style: "mapbox://styles/mapbox/dark-v10"
     });
 
-    // Expose mapController globalement pour l'utiliser dans `search_controller.js`
     window.mapController = this;
 
-    this.markersMap = {}; // Stockage des marqueurs avec les noms des mercenaires
+    this.markersMap = {};
+
     this.addMarkersToMap();
     this.fitMapToMarkers();
   }
 
   addMarkersToMap() {
-    this.markersMap = {}; // Réinitialise les marqueurs
+    this.markersMap = {};
 
     this.markersValue.forEach(marker => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window);
 
-      const newMarker = new mapboxgl.Marker()
+      const newMarker = new mapboxgl.Marker({ color: "#1f3148" })
         .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
         .addTo(this.map);
 
-      // Stocke les marqueurs pour la recherche
-      this.markersMap[marker.name.toLowerCase()] = newMarker;
+      const normalizedName = marker.name.toLowerCase().trim();
+      this.markersMap[normalizedName] = newMarker;
     });
 
-    console.log("Marqueurs chargés :", this.markersMap); // Vérification dans la console
+    console.log("Marqueurs chargés :", this.markersMap);
   }
 
   fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds();
-    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+    this.markersValue.forEach(marker => {
+      bounds.extend([marker.lng, marker.lat]);
+    });
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
 }
